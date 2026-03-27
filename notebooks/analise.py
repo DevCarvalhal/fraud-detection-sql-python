1a6e72b3b87ab16f4c5129117120f98fd36820eb
Vitor Carvalhal <devcarvalhal@gmail.com>
2026-03-27 15:35:32 -0300

    melhora analise comtotal e percentual de transacoes suspeitas

--- notebooks/analise.py
+++ notebooks/analise.py
@@ -2,7 +2,12 @@ import pandas as pd
 
 df = pd.read_csv('data/transacoes.csv')
 
-suspeitas = df[(df['valor'] > 5000) | (df['score_risco'] > 80)]
+suspeitas = df[(df['valor'] >= 5000) | (df['score_risco'] > 80)]
 
-print("Transações suspeitas:")
+print("Total de transações:", len(df))
+print("Total de transações suspeitas:", len(suspeitas))
+print("\nTransações suspeitas:")
 print(suspeitas)
+
+percentual = (len(suspeitas) / len(df)) * 100
+print(f"\nPercentual de transações suspeitas: {percentual:.2f}%")
